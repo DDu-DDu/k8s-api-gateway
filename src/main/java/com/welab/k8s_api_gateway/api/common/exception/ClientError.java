1a3e4adb382419c96239cc1f260dd01f56633683
lyg0527 <lyg0527@navre.com>
2025-06-11 15:24:51 +0900

    Initial commit

---
+++ src/main/java/com/welab/k8s_api_gateway/api/common/exception/ClientError.java
@@ -0,0 +1,8 @@
+package com.welab.k8s_api_gateway.api.common.exception;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ClientError extends ApiError {
+}
---
+++ src/main/java/com/welab/k8s_api_gateway/api/common/exception/NotFound.java
@@ -0,0 +1,8 @@
+package com.welab.k8s_api_gateway.api.common.exception;
+
+public class NotFound extends ClientError {
+    public NotFound(String message) {
+        this.errorCode = "NotFound";
+        this.errorMessage = message;
+    }
+}
